be32ac844493116b1154d5c43555c91c9e3b86c2
huscse <husnainkhaliq82@gmail.com>
2025-04-19 15:22:31 -0400

    updated ui

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,27 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "PromptWise",
-  description: "an AI-Powered flashcards generator",
+  title: 'PromptWise',
+  description: 'an AI-Powered flashcards generator',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider> 
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <script
+            defer
+            src="https://cloud.umami.is/script.js"
+            data-website-id="14193676-add3-4ddf-b12a-a48efc57e9cf"
+          ></script>
+        </head>
+        <body className={inter.className}>{children}</body>
+      </html>
     </ClerkProvider>
   );
 }
